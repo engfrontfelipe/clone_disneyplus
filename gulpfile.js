76dd60f0dd3ce0d6b8c961ebb8c0a081a1ac1ece
Felipe Maciel <engfrontfelipe@gmail.com>
2024-07-21 14:23:43 -0300

    commit teste

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,22 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
+const imagemin = require('gulp-imagemin');
+
 
 function styles() {
     return gulp.src('./src/styles/*.scss')
         .pipe(sass({outputStyle: 'compressed'}))
-            .pipe(gulp.dest('./dist/css'));
-}
+        .pipe(gulp.dest('./dist/css'));
+};
 
-exports.default = styles;
+function imagens() {
+    return gulp.src('./src/images/**/*')
+        .pipe(gulp.dest('./dist/imagemin'));
+};
+
+
+
+exports.default = gulp.series(styles, imagens);
 exports.watch = function(){
     gulp.watch('./src/styles/*.scss', gulp.parallel(styles))
-}
+};
